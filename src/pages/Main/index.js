2e8bc3f0075d13d10bff9b38322d3327eee3c471
Eric Patrick <ericpatrick15@gmail.com>
2018-03-19 22:42:01 -0300

    Trying configure react native maps

---
+++ src/pages/Main/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import { View } from 'react-native';
+import Map from './components/Map';
+
+// import styles from './styles';
+
+const Main = () => (
+  <Map/>
+);
+
+export default Main;
