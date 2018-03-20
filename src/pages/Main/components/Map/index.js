7cf23366265a09445a72a5abb1782f9d6ad25aa6
Eric Patrick <ericpatrick15@gmail.com>
2018-03-19 23:01:21 -0300

    Map style

--- src/pages/Main/components/Map/index.js
+++ src/pages/Main/components/Map/index.js
@@ -2,10 +2,11 @@ import React from 'react';
 
 import MapView from 'react-native-maps';
 
-// import styles from './styles';
+import styles from './styles';
 
 const Map = () => (
   <MapView
+    style={styles.map}
     initialRegion={{
       latitude: 37.78825,
       longitude: -122.4324,
